de79b43ef75dbebb68b16d874bf05985c7e91872
kaministrator999-ui <kaministrator999@gmail.com>
2026-09-08 18:36:14 -0500

    Base 2.2.7: build native R39 batched prefill extension

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import Extension, setup
 import numpy
 
+COMMON_ARGS = ["-O3", "-ffast-math", "-fno-math-errno", "-funroll-loops"]
+
 setup(
     name="swrlzkamico-native",
-    version="0.1.0",
+    version="0.2.0",
     packages=["swyrlz"],
     ext_modules=[
         Extension(
             "swyrlz._r39_native",
             sources=["native/r39_native.c"],
             include_dirs=[numpy.get_include()],
-            extra_compile_args=["-O3", "-ffast-math", "-fno-math-errno", "-funroll-loops"],
-        )
+            extra_compile_args=COMMON_ARGS,
+        ),
+        Extension(
+            "swyrlz._r39_batch",
+            sources=["native/r39_batch.c"],
+            include_dirs=[numpy.get_include()],
+            extra_compile_args=COMMON_ARGS,
+        ),
     ],
 )
