0f1fd68b0c2fc552a354b4495c3d677763a1f991
kaministrator999-ui <kaministrator999@gmail.com>
2026-09-19 14:31:01 -0500

    perf(chat): stop idle hot-status polling

--- web/chat_account.js
+++ web/chat_account.js
@@ -20,5 +20,5 @@ Promise.resolve()
   .then(()=>load("chat_stream_focus.js"))
   .then(()=>refreshIfChanged())
   .catch(error=>console.warn("§wyrlz hot chat assets unavailable",error));
-setInterval(refreshIfChanged,5000);
+document.addEventListener("visibilitychange",()=>{if(!document.hidden)refreshIfChanged()});
 })();
