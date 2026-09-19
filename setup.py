36dbab73ea7dbc5e0a430416a987af155a1f067a
kaministrator999-ui <kaministrator999@gmail.com>
2026-09-19 10:26:03 -0500

    build(r39): enable OpenMP accelerator build

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 from setuptools import Extension, setup
+import os
 import numpy
 
 COMMON_ARGS = ["-O3", "-ffast-math", "-fno-math-errno", "-funroll-loops"]
+USE_OPENMP = os.environ.get("SWYRLZ_OPENMP", "1") not in {"0", "false", "False"}
+OPENMP_COMPILE = ["-fopenmp"] if USE_OPENMP else []
+OPENMP_LINK = ["-fopenmp"] if USE_OPENMP else []
 
 setup(
     name="swrlzkamico-native",
-    version="0.2.0",
+    version="0.2.1",
     packages=["swyrlz"],
     ext_modules=[
         Extension(
             "swyrlz._r39_native",
             sources=["native/r39_native.c"],
             include_dirs=[numpy.get_include()],
-            extra_compile_args=COMMON_ARGS,
+            extra_compile_args=COMMON_ARGS + OPENMP_COMPILE,
+            extra_link_args=OPENMP_LINK,
         ),
         Extension(
             "swyrlz._r39_batch",
             sources=["native/r39_batch.c"],
             include_dirs=[numpy.get_include()],
-            extra_compile_args=COMMON_ARGS,
+            extra_compile_args=COMMON_ARGS + OPENMP_COMPILE,
+            extra_link_args=OPENMP_LINK,
         ),
     ],
 )
